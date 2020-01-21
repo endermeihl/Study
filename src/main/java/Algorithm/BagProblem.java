c92488c438e315fdc0b8dfd65b1da1c540f9ae97
endermeihl <endermiehl@gmail.com>
2020-01-21 20:26:45 +0800

    数据结构和算法

--- src/main/java/Algorithm/BagProblem.java
+++ src/main/java/Algorithm/BagProblem.java
@@ -1,4 +1,24 @@
 package Algorithm;
 
 public class BagProblem {
+    public int maxPrice(int[] price, int[] weight, int w, int n) {
+        int[][] dp = new int[n + 1][w + 1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < w + 1; j++) {
+                if (weight[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i - 1]] + price[i - 1]);
+                }
+            }
+        }
+        return dp[n][w];
+    }
+
+    public static void main(String[] args) {
+        BagProblem bagProblem = new BagProblem();
+        int[] price = new int[]{1500, 3000, 2000, 2000};
+        int[] weight = new int[]{1, 4, 3, 1};
+        System.out.println(bagProblem.maxPrice(price, weight, 4, 4));
+    }
 }

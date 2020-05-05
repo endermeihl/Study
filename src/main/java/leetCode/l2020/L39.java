b02ce214918956ba6f9f752082598ad30d51a325
endermeihl <endermiehl@gmail.com>
2020-05-05 09:58:33 +0800

    # lettcode每日刷题 ## 每日刷题 争取一天刷5-10题

---
+++ src/main/java/leetCode/l2020/L39.java
@@ -0,0 +1,73 @@
+package leetCode.l2020;
+
+import edu.princeton.cs.algs4.In;
+import leetCode.l2020.type.ArrayProblem;
+import leetCode.l2020.type.Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author ender
+ * @version 39
+ * 给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+ * candidates 中的数字可以无限制重复被选取。
+ * 说明：
+ * 所有数字（包括 target）都是正整数。
+ * 解集不能包含重复的组合。 
+ * 示例 1:
+ * 输入: candidates = [2,3,6,7], target = 7,
+ * 所求解集为:
+ * [
+ * [7],
+ * [2,2,3]
+ * ]
+ * 示例 2:
+ * 输入: candidates = [2,3,5], target = 8,
+ * 所求解集为:
+ * [
+ *   [2,2,2,2],
+ *   [2,3,3],
+ *   [3,5]
+ * ]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combination-sum
+ */
+public class L39 implements ArrayProblem, Backtracking {
+    List<List<Integer>> res = new ArrayList<>();
+    int[] candidates;
+    int len;
+
+    /**
+     * @param candidates 传入数组
+     * @param target     目标和
+     * @return 满足条件的结果集
+     * 使用回溯发标准模板处理 这里使用了栈
+     * 需要注意的是这里不允许重复
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        len = candidates.length;
+        if (len == 0) {
+            res.add(new ArrayList<>());
+            return res;
+        }
+        this.candidates = candidates;
+        Arrays.sort(candidates);
+        helper(target, 0, new Stack<>());
+        return res;
+    }
+
+    private void helper(int pre, int start, Stack<Integer> stack) {
+        if (pre == 0) {
+            res.add(new ArrayList<>(stack));
+            return;
+        }
+        for (int i = start; i < len && pre - candidates[i] >= 0; i++) {
+            stack.push(candidates[i]);
+            helper(pre - candidates[i], i, stack);
+            stack.pop();
+        }
+    }
+}
---
+++ src/main/java/leetCode/l2020/L98.java
@@ -0,0 +1,50 @@
+package leetCode.l2020;
+
+import base.TreeNode;
+import leetCode.l2020.type.DFSProblem;
+import leetCode.l2020.type.TreeNodeProblem;
+
+/**
+ * @author ender
+ * @version 98
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 假设一个二叉搜索树具有如下特征：
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ */
+public class L98 implements TreeNodeProblem, DFSProblem {
+    /**
+     * @param root 输入搜索树
+     * @return 判断结果
+     * 深度优先遍历
+     */
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    private boolean helper(TreeNode root, Integer min, Integer max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+}

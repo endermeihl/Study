1202b8681537457d3a8289a7409de9c4a1a901e9
ender <endermeihl@gmail.com>
2020-07-22 15:11:18 +0800

    shell脚本

---
+++ shell/echo_color.sh
@@ -0,0 +1,14 @@
+function echo_color() {
+    case $1 in
+        green)
+            echo "\033[32;40m$2\033[0m"
+	    ;;
+        red)
+            echo "\033[31;40m$2\033[0m"
+            ;;
+        *) 
+            echo "Example: echo_color red string"
+    esac
+}
+
+echo_color $1 $2
